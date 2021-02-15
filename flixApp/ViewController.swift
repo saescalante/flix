5c448d2ef329c57837945a4ed9213e3db0122eef
Jimmy Escalante <escalasa@oregonstate.edu>
2021-02-14 18:35:39 -0600

    Initial Commit

---
+++ flixApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  flixApp
+//
+//  Created by Santiago Escalante  on 2/14/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
