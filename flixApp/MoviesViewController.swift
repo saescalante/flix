363d5d2dad3e4489a26c2a56c645cca15257649c
Jimmy Escalante <escalasa@oregonstate.edu>
2021-02-19 19:14:32 -0600

    Created custom cells to include title & synopsis.

--- flixApp/MoviesViewController.swift
+++ flixApp/MoviesViewController.swift
@@ -48,11 +48,14 @@ class MoviesViewController: UIViewController, UITableViewDataSource, UITableView
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = UITableViewCell()
+        let cell = tableView.dequeueReusableCell(withIdentifier: "MovieCell") as! MovieCell
         let movie = movies[indexPath.row]
         let title = movie["title"] as! String
-        cell.textLabel?.text = title
+        let synopsis = movie["overview"] as! String
         
+        cell.titleLabel.text = title
+        cell.synopsisLabel.text = synopsis
+         
         return cell
     }
     
